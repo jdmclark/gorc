5edcf4b7d0d07c020914dd92ab3c245ada1000bd
shinyquagsire23 <mtinc2@gmail.com>
2015-04-29 21:41:27 -0700

    Don't scale jump thrust along with x and y thrust

--- src/game/world/aspects/character_controller_aspect.cpp
+++ src/game/world/aspects/character_controller_aspect.cpp
@@ -162,26 +162,30 @@ void character_controller_aspect::update_standing(entity_id thing_id, components
         }
 
         //Adjust thrust depending on action
+        float scale_adjust = 1.0f;
         if(thing.physics_flags & flags::physics_flag::is_crouching) {
-            thing.thrust *= ((float)4 / (float)15);
+        	scale_adjust *= ((float)4 / (float)15);
         }
         else {
             if(fabs(vel_lr) > fabs(vel_fb)) {
-                thing.thrust *= 0.5f;
+            	scale_adjust *= 0.5f;
             }
             else {
-                thing.thrust *= ((float)2 / (float)3);
+            	scale_adjust *= ((float)2 / (float)3);
             }
 
             if(thing.physics_flags & flags::physics_flag::is_running) {
-                thing.thrust *= 2;
+            	scale_adjust *= 2;
             }
         }
 
         //Walking backwards
         if(vel_fb < 0.0f && fabs(vel_lr) < fabs(vel_fb)) {
-            thing.thrust *= 0.5f;
+        	scale_adjust *= 0.5f;
         }
+
+        get<0>(thing.thrust) *= scale_adjust;
+		get<1>(thing.thrust) *= scale_adjust;
 
         if(get<2>(thing.thrust) > 0.0f) {
             jump(thing_id, thing);
