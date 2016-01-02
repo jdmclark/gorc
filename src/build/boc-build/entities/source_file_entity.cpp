4af9813fa428c98a29181788059926da644c0b38
Jonathan Clark <jon@nullptr.ca>
2016-01-02 00:21:37 -0800

    Refactored source file entity serialization

--- src/build/boc-build/entities/source_file_entity.cpp
+++ src/build/boc-build/entities/source_file_entity.cpp
@@ -1,10 +1,23 @@
 #include "source_file_entity.hpp"
 #include "build/boc-build/engine/entity_serializer.hpp"
+#include "build/boc-build/engine/entity_deserializer.hpp"
+
+gorc::source_file_entity::source_file_entity(entity_input_stream &is)
+    : file_entity(is)
+{
+    return;
+}
+
+gorc::source_file_entity::source_file_entity(path const &new_filename,
+                                             std::time_t previous_timestamp)
+    : file_entity(new_filename, previous_timestamp)
+{
+    return;
+}
 
 void gorc::source_file_entity::serialize(entity_output_stream &os)
 {
-    os.write_path(filename);
-    os.write_uint32(static_cast<uint32_t>(previous_timestamp));
+    file_entity::serialize(os);
 }
 
 std::type_index gorc::source_file_entity::get_type_index() const
--- src/build/boc-build/entities/source_file_entity.hpp
+++ src/build/boc-build/entities/source_file_entity.hpp
@@ -6,7 +6,9 @@ namespace gorc {
 
     class source_file_entity : public file_entity {
     public:
-        using file_entity::file_entity;
+        source_file_entity(entity_input_stream &);
+        source_file_entity(path const &filename,
+                           std::time_t previous_timestamp = 0);
 
         virtual void serialize(entity_output_stream &) override;
 
